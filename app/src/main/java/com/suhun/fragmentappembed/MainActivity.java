e97d5128ce81a63ef2ed54b978570cb01c381338
Shu-Hung Lin <suhunlin0310@gmail.com>
2023-05-11 12:25:10 +0800

    Add Main activity layout and init button

--- app/src/main/java/com/suhun/fragmentappembed/MainActivity.java
+++ app/src/main/java/com/suhun/fragmentappembed/MainActivity.java
@@ -1,14 +1,34 @@
 package com.suhun.fragmentappembed;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    public String tag = MainActivity.class.getSimpleName();
+    Button goPage1Btn, goPage2Btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView(){
+        goPage1Btn = findViewById(R.id.f1Page);
+        goPage2Btn = findViewById(R.id.f2Page);
+    }
+
+    public void goPage1Fun(View view){
+
+    }
+
+    public void goPage2Fun(View view){
+
     }
 }
