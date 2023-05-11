3cbbecc5456d68115045b75a047a8fbfb7c5857a
Shu-Hung Lin <suhunlin0310@gmail.com>
2023-05-11 12:33:41 +0800

    Modify some code logic for view

--- app/src/main/java/com/suhun/fragmentappembed/F1Fragment.java
+++ app/src/main/java/com/suhun/fragmentappembed/F1Fragment.java
@@ -1,18 +1,31 @@
 package com.suhun.fragmentappembed;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 public class F1Fragment extends Fragment {
+    private View view;
+    private MainActivity activity;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_f1, container, false);
+        if(view==null){
+            // Inflate the layout for this fragment
+            view = inflater.inflate(R.layout.fragment_f1, container, false);
+        }
+        return view;
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        activity = (MainActivity)context;
     }
 }
--- app/src/main/java/com/suhun/fragmentappembed/F2Fragment.java
+++ app/src/main/java/com/suhun/fragmentappembed/F2Fragment.java
@@ -1,7 +1,9 @@
 package com.suhun.fragmentappembed;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -9,10 +11,21 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class F2Fragment extends Fragment {
+    private View view;
+    private MainActivity activity;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_f2, container, false);
+        if(view==null){
+            // Inflate the layout for this fragment
+            view = inflater.inflate(R.layout.fragment_f2, container, false);
+        }
+        return view;
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        activity = (MainActivity)context;
     }
 }
