e05cceb11e44a9e86ea14f563d6be9eec8e76e54
Shu-Hung Lin <suhunlin0310@gmail.com>
2023-05-11 11:47:08 +0800

    Add F1 fragment layout

---
+++ app/src/main/java/com/suhun/fragmentappembed/SignView.java
@@ -0,0 +1,13 @@
+package com.suhun.fragmentappembed;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class SignView extends View {
+    public SignView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
