ed07343b83244e89bd85efc1acd37ce90fb2d037
Shu-Hung Lin <suhunlin0310@gmail.com>
2023-05-11 12:29:34 +0800

    Add code to init fragment

--- app/src/main/java/com/suhun/fragmentappembed/MainActivity.java
+++ app/src/main/java/com/suhun/fragmentappembed/MainActivity.java
@@ -10,18 +10,31 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     public String tag = MainActivity.class.getSimpleName();
-    Button goPage1Btn, goPage2Btn;
+    private Button goPage1Btn, goPage2Btn;
+    private F1Fragment f1;
+    private F2Fragment f2;
+    private FragmentManager fmgr;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
+        initFrigment();
     }
-
     private void initView(){
         goPage1Btn = findViewById(R.id.f1Page);
         goPage2Btn = findViewById(R.id.f2Page);
+    }
+
+    private void initFrigment(){
+        f1 = new F1Fragment();
+        f2 = new F2Fragment();
+        fmgr = getSupportFragmentManager();
+        FragmentTransaction transaction = fmgr.beginTransaction();
+        transaction.add(R.id.container, f1);
+        transaction.commit();
     }
 
     public void goPage1Fun(View view){
