61a93e06c24527bc7151d57755f430188fc5d55f
Shu-Hung Lin <suhunlin0310@gmail.com>
2023-05-11 12:58:06 +0800

    Implement draw lines logic

--- app/src/main/java/com/suhun/fragmentappembed/SignView.java
+++ app/src/main/java/com/suhun/fragmentappembed/SignView.java
@@ -10,6 +10,9 @@ import android.view.View;
 
 import androidx.annotation.Nullable;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+
 public class SignView extends View {
     private MainActivity activity;
     private AppData.SignViewData signViewData;
@@ -26,10 +29,27 @@ public class SignView extends View {
         Paint paint = new Paint();
         paint.setColor(Color.BLUE);
         paint.setStrokeWidth(4);
+        for(LinkedList<HashMap<String, Float>> line:signViewData.getLines()){
+            for(int i=1; i<line.size();i++){
+                HashMap<String, Float> p0 = line.get(i-1);
+                HashMap<String, Float> p1 = line.get(i);
+                canvas.drawLine(p0.get("x"), p0.get("y"), p1.get("x"), p1.get("y"), paint);
+            }
+        }
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
+        HashMap<String, Float> point = new HashMap<>();
+        point.put("x", event.getX()); point.put("y", event.getY());
+        if(event.getAction()==MotionEvent.ACTION_DOWN){
+            LinkedList<HashMap<String, Float>> line = new LinkedList<>();
+            line.add(point);
+            signViewData.getLines().add(line);
+        }else if(event.getAction()==MotionEvent.ACTION_MOVE){
+            signViewData.getLines().getLast().add(point);
+        }
+        invalidate();
         return true;
     }
 }
