a242115a589466cb9f79762dc2b8584405b4ebdb
Shu-Hung Lin <suhunlin0310@gmail.com>
2023-05-11 12:49:59 +0800

    Add onDraw and onTouchEvent

--- app/src/main/java/com/suhun/fragmentappembed/SignView.java
+++ app/src/main/java/com/suhun/fragmentappembed/SignView.java
@@ -1,7 +1,11 @@
 package com.suhun.fragmentappembed;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 import androidx.annotation.Nullable;
@@ -14,5 +18,18 @@ public class SignView extends View {
         activity = (MainActivity) context;
         AppData appData = (AppData)activity.getApplication();
         signViewData = appData.signViewData;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        Paint paint = new Paint();
+        paint.setColor(Color.BLUE);
+        paint.setStrokeWidth(4);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return true;
     }
 }
